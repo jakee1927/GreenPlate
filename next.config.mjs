98b873d7a902d18e363a370990a3e6a1ddcbcca5
jakee1927 <jakee8228@gmail.com>
2025-03-31 15:52:52 -0400

    enabled error reporting

--- next.config.mjs
+++ next.config.mjs
@@ -8,10 +8,8 @@ try {
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    ignoreDuringBuilds: true,
   },
   typescript: {
-    ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
